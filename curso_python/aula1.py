c03dee2a1425419eae3c411139a9138d8a29d20f
Daniel Trombetta <145787463+DanielTrombetta08@users.noreply.github.com>
2026-03-11 21:03:37 -0300

    Initial commit

---
+++ curso_python/aula1.py
@@ -0,0 +1,2 @@
+# Permite escrever um comentário
+print(123)
---
+++ curso_python/aula3.py
@@ -0,0 +1,23 @@
+'''
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+
+'''
+print(134)
+
+# Aspas simples
+print('Daniel')
+print('Daniel "Trombetta"')
+
+
+# Aspas duplas
+print("Daniel")
+
+# Escape
+print("Daniel \"Trombetta\"")
+
+# r 
+print(r"Daniel \"Trombetta\"")
